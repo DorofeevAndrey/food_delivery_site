bf52fcfb85ec5ba418ec82acbcfc810ea502a587
Andrey <dorofeevandrey78@gmail.com>
2025-09-08 17:14:08 +0500

    feat: edit User table

--- back_fastapi/app/models/user.py
+++ back_fastapi/app/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, DateTime, Boolean, func
+from sqlalchemy import Column, Date, Integer, String, DateTime, Boolean, func
 from sqlalchemy.orm import relationship
 from datetime import datetime
 from app.core.database import Base
@@ -18,9 +18,11 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
 
     # Аутентификация
-    phone = Column(String, unique=True, index=True, nullable=False)   # вход по SMS
-    telegram_id = Column(String, unique=True, index=True, nullable=True)  # вход через Telegram
+    phone = Column(String, unique=True, index=True, nullable=False)   
+    telegram_id = Column(String, unique=True, index=True, nullable=True)  
 
     # Личные данные
     first_name = Column(String, nullable=True)
+    date_of_birth = Column(Date, nullable=True)
     email = Column(String, unique=True, index=True, nullable=True)
+    gender = Column(String, nullable=True)
