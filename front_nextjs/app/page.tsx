1a79b78160a058bd6716b5bf7bae6151df1d484e
Andrey <dorofeev_andrey03@bk.ru>
2026-02-10 23:44:21 +0500

    fix: fix some problem and create main

--- front_nextjs/app/page.tsx
+++ front_nextjs/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
 import Header from "@/components/Organizm/Header/Header";
+import Main from "@/components/Organizm/Main/Main";
 import ProductList from "@/components/Organizm/ProductList/ProductList";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <ProductList />
+      <Main />
     </>
   );
 }
